31b333d49ecc17f4979d7421bc4dea791dd751e0
Dmitriy <d.gnatishen@gmail.com>
2022-08-13 18:19:26 +0300

    (front) Create axios interceptors

--- front/app/api/interceptors.ts
+++ front/app/api/interceptors.ts
@@ -1,6 +1,12 @@
 import axios from 'axios'
+import { Cookies } from 'js-cookie'
+
+import { AuthService } from '@/services/auth/auth.service'
 
 import { API_URL } from '@/configs/api.config'
+
+import { removeTokensStorage } from './../services/auth/auth.halper'
+import { errorCatch, getContentType } from './api.helpers'
 
 export const axiosClassic = axios.create({
 	baseURL: API_URL,
@@ -8,3 +14,45 @@ export const axiosClassic = axios.create({
 		'Content-Type': 'application/json',
 	},
 })
+
+export const instance = axios.create({
+	baseURL: API_URL,
+	headers: getContentType(),
+})
+
+instance.interceptors.request.use((config) => {
+	const accessToken = Cookies.get('accessToken')
+
+	if (config.headers && accessToken)
+		config.headers.Authorization = `Bearer ${accessToken}`
+
+	return config
+})
+
+instance.interceptors.response.use(
+	(config) => config,
+	async (error) => {
+		const originalRequest = error.config
+
+		if (
+			(error.response.status === 401 ||
+				errorCatch(error) === 'jwt expired' ||
+				errorCatch(error) === 'jwt must be provided') &&
+			error.config &&
+			!error.config._isRetry
+		) {
+			originalRequest._isRetry = true
+			try {
+				await AuthService.getNewTokens()
+
+				return instance.request(originalRequest)
+			} catch (e) {
+				if (errorCatch(e) === 'jwt expired') removeTokensStorage()
+			}
+		}
+
+		throw error
+	}
+)
+
+export default instance
